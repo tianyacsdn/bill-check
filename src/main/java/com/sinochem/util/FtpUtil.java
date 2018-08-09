4432f27b0a03d1af5b48339682d19848efa9635b
tom <tianyacsdn@126.com>
2018-08-09 14:14:01 +0800

    merge FTP service code

--- src/main/java/com/sinochem/util/FtpUtil.java
+++ src/main/java/com/sinochem/util/FtpUtil.java
@@ -29,18 +29,18 @@ public class FtpUtil {
                                   String hostname,
                                   Integer port,
                                   String username,
-                                  String password) {
+                                  String password) throws Exception {
         if (!ftpClient.isConnected()) {
             try {
                 ftpClient.connect(hostname, port);
             } catch (IOException e) {
-                System.out.println("连接FTP服务器失败："+e.getMessage());
+                throw new Exception("连接FTP服务器失败：" + e.getMessage());
             }
         }
         try {
             ftpClient.login(username, password);
         } catch (IOException e) {
-            System.out.println("登录FTP服务器失败："+e.getMessage());
+            throw new Exception("登录FTP服务器失败："+e.getMessage());
         }
         int reply = ftpClient.getReplyCode();
         if (!FTPReply.isPositiveCompletion(reply)) {
@@ -53,7 +53,7 @@ public class FtpUtil {
      * 断开FTP服务器
      * @param ftpClient
      */
-    public static void disconnect(FTPClient ftpClient) {
+    public static void disconnect(FTPClient ftpClient) throws Exception {
         try {
             if (ftpClient != null) {
                 if (ftpClient.isConnected()) {
@@ -63,7 +63,7 @@ public class FtpUtil {
                 ftpClient = null;
             }
         } catch (IOException e) {
-            System.out.println("断开FTP服务器异常："+e.getMessage());
+            throw new Exception("断开FTP服务器异常："+e.getMessage());
         }
     }
 
@@ -84,39 +84,7 @@ public class FtpUtil {
                                           String password,
                                           String ftpPath,
                                           String localPath) throws Exception {
-        FTPClient ftpClient = new FTPClient();
-        LocalDate localDate = LocalDate.now().minusDays(1);
-        String date = localDate.toString().replace("-","");
-        //连接FTP服务器
-        connect(ftpClient,hostname,port,username,password);
-        List<String> list = new ArrayList<>();
-        OutputStream os = null;
-        if (!ftpClient.changeWorkingDirectory(ftpPath)) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录不存在");
-        }
-        ftpClient.enterLocalPassiveMode();//设置被动模式，开通一个端口来接收数据
-        String[] files = ftpClient.listNames();
-        if (files == null || files.length == 0) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录为空！");
-        }
-        FTPFile[] ftpFiles = ftpClient.listFiles();
-
-        for (FTPFile file: ftpFiles) {
-            String fileName = file.getName();
-            if (fileName.substring(2,10).equals(date)) {
-                File localFile = new File(localPath + File.separator + fileName);
-                os = new FileOutputStream(localFile);
-                ftpClient.retrieveFile(file.getName(), os);
-                list.add(fileName);
-            }
-        }
-        if (os != null) {
-            os.close();
-        }
-        //断开FTP服务器
-        disconnect(ftpClient);
+        List<String> list = downloadBill("", "", hostname, port, username, password, ftpPath,localPath );
         return list;
     }
 
@@ -139,37 +107,7 @@ public class FtpUtil {
                                       String password,
                                       String ftpPath,
                                       String localPath) throws Exception {
-        FTPClient ftpClient = new FTPClient();
-        //连接FTP服务器
-        connect(ftpClient,hostname,port,username,password);
-        List<String> list = new ArrayList<>();
-        OutputStream os = null;
-        if (!ftpClient.changeWorkingDirectory(ftpPath)) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录不存在");
-        }
-        ftpClient.enterLocalPassiveMode();//设置被动模式，开通一个端口来接收数据
-        String[] files = ftpClient.listNames();
-        if (files == null || files.length == 0) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录为空！");
-        }
-        FTPFile[] ftpFiles = ftpClient.listFiles();
-
-        for (FTPFile file: ftpFiles) {
-            String fileName = file.getName();
-            if (fileName.substring(2,10).equals(date)) {
-                File localFile = new File(localPath + File.separator + fileName);
-                os = new FileOutputStream(localFile);
-                ftpClient.retrieveFile(file.getName(), os);
-                list.add(fileName);
-            }
-        }
-        if (os != null) {
-            os.close();
-        }
-        //断开FTP服务器
-        disconnect(ftpClient);
+        List<String> list = downloadBill(date, "", hostname, port, username, password, ftpPath,localPath );
         return list;
     }
 
@@ -192,39 +130,7 @@ public class FtpUtil {
                                                  String password,
                                                  String ftpPath,
                                                  String localPath) throws Exception{
-        FTPClient ftpClient = new FTPClient();
-        LocalDate localDate = LocalDate.now().minusDays(1);
-        String date = localDate.toString().replace("-","");
-        //连接FTP服务器
-        connect(ftpClient,hostname,port,username,password);
-        List<String> list = new ArrayList<>();
-        OutputStream os = null;
-        if (!ftpClient.changeWorkingDirectory(ftpPath)) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录不存在");
-        }
-        ftpClient.enterLocalPassiveMode();//设置被动模式，开通一个端口来接收数据
-        String[] files = ftpClient.listNames();
-        if (files == null || files.length == 0) {
-            disconnect(ftpClient);
-            throw new Exception("对账单下载失败，目录为空！");
-        }
-        FTPFile[] ftpFiles = ftpClient.listFiles();
-
-        for (FTPFile file: ftpFiles) {
-            String fileName = file.getName();
-            if (fileName.substring(2,10).equals(date) && fileName.substring(0,2).equals(billType)) {
-                File localFile = new File(localPath + File.separator + fileName);
-                os = new FileOutputStream(localFile);
-                ftpClient.retrieveFile(file.getName(), os);
-                list.add(fileName);
-            }
-        }
-        if (os != null) {
-            os.close();
-        }
-        //断开FTP服务器
-        disconnect(ftpClient);
+        List<String> list = downloadBill("", billType, hostname, port, username, password, ftpPath,localPath );
         return list;
     }
     /**
@@ -248,6 +154,22 @@ public class FtpUtil {
                                              String password,
                                              String ftpPath,
                                              String localPath) throws Exception {
+        List<String> list = downloadBill(date, billType, hostname, port, username, password, ftpPath,localPath );
+        return list;
+    }
+
+    public static List<String> downloadBill(String date,
+                                     String billType,
+                                     String hostname,
+                                     Integer port,
+                                     String username,
+                                     String password,
+                                     String ftpPath,
+                                     String localPath) throws Exception {
+        if ("".equals(date) || date == null) {
+            LocalDate localDate = LocalDate.now().minusDays(1);
+            date = localDate.toString().replace("-","");
+        }
         FTPClient ftpClient = new FTPClient();
         //连接FTP服务器
         connect(ftpClient,hostname,port,username,password);
@@ -267,7 +189,14 @@ public class FtpUtil {
 
         for (FTPFile file: ftpFiles) {
             String fileName = file.getName();
-            if (fileName.substring(2,10).equals(date) && fileName.substring(0,2).equals(billType)) {
+            boolean conditionFlag = false;
+            //billType不为空，下载指定类型的对账单;为空，下载所有类型的对账单
+            if (!"".equals(billType) && billType != null) {
+                conditionFlag = fileName.substring(0,2).equals(billType);
+            } else {
+                conditionFlag = true;
+            }
+            if (fileName.substring(2,10).equals(date) && conditionFlag) {
                 File localFile = new File(localPath + File.separator + fileName);
                 os = new FileOutputStream(localFile);
                 ftpClient.retrieveFile(file.getName(), os);
@@ -281,7 +210,6 @@ public class FtpUtil {
         disconnect(ftpClient);
         return list;
     }
-
     public static void main(String[] args) throws Exception{
 //        FtpUtil.downloadT1("10.144.137.47",21,"payhadmin",
 //                "DgWai2=FoCB7","/home/wasadmin","ftp-file");
